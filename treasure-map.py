dabf2fa6739307c8f7c59b9c0dad32ad2da64db7
ekioko <43186178+ekioko@users.noreply.github.com>
2020-12-09 16:34:05 -0500

    Create treasure-map.py

---
+++ treasure-map.py
@@ -0,0 +1,17 @@
+row1 = ["⬜️","⬜️","⬜️"]
+row2 = ["⬜️","⬜️","⬜️"]
+row3 = ["⬜️","⬜️","⬜️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure? ")
+
+co_ord = []
+for char in position:
+  co_ord.append(char)
+
+across = int(co_ord[0])-1
+down = int(co_ord[1])-1
+map[across][down] = 'X'
+
+print(f"{row1}\n{row2}\n{row3}")
